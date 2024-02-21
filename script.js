9bbd84c198a77f966d3948caa95d4204f4bd4969
Giridharan-ranganathan <giridharan.ranganathan.99@gmail.com>
2024-02-21 11:51:27 +0530

    creating form using DOM

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-var div = document.createElement("div");
-div.className = "col";
+function label_create(tagname,attrname,attvalue,content){
+    var ele =document.createElement(tagname);
+    ele.setAttribute(attrname,attvalue);
+    ele.innerHtml=content
+    return ele;
+}
 
-var firstName = document.createElement("label");
-firstName.innerHTML = "FirstName";
+function break_create(){
+    var ele = document.createElement("br");
+    return ele;
+}
 
-var middleName = document.createElement("label");
-middleName.innerHTML = "middleName";
-
-var lastName = document.createElement("label");
-lastName.innerHTML = "lastName";
-
-var email = document.createElement("label");
-email.innerHTML = "email";
+function input_create(tagname,attr1name,attr1value,attr2name,attr2value){
+    var element = document.createElement(tagname);
+    element.setAttribute(attr1name,attr1value);
+    element.setAttribute(attr2name,attr2value);
+    return element;
+}
 
 
-div.append(firstName,middleName,lastName,email);
-document.body.append(div);
-console.log(div);
+var firstname =label_create("label","for","firstname","FirstName");
+var br = break_create();
+var firstnameInpute = input_create("input","type","firstname","id","firstname");
+
+var middlename =label_create("label","for","middlename","middlename");
+var br = break_create();
+var middlenameInput = input_create("input","type","middlename","id","middlename");
+
+var lastname =label_create("label","for","lastname","lastname");
+var br = break_create();
+var lastnameInput = input_create("input","type","lastname","id","lastname");
+
+var email =label_create("label","for","email","Email");
+var br = break_create();
+var emailInput = input_create("input","type","email","id","email");
+
+var password =label_create("label","for","password","password");
+var br = break_create();
+var passwordInput = input_create("input","type","password","id","password");
+
+document.body.append(firstname,br,firstnameInpute);
